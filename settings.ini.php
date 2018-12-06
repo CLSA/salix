aefea201fc8bcf5c0b51e32a579aec4b7a19dabb
Patrick Emond <emondpd@mcmaster.ca>
2018-12-06 14:25:33 -0500

    Removing invalid code from an old copy/paste

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'salix';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.3';
-$SETTINGS['general']['build'] = '7b2f0cd';
+$SETTINGS['general']['build'] = '275286a';
 
 // the location of salix internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
