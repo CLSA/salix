3e2c4d6e808e1250e2c30ff88df83e67e4e11b21
Patrick Emond <emondpd@mcmaster.ca>
2019-08-30 16:21:24 -0400

    Marking the start of version 2.5

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'salix';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.4';
+$SETTINGS['general']['version'] = '2.5';
 $SETTINGS['general']['build'] = '13ec295';
 
 // the location of salix internal path
