3f68211bc6d6d794663c603429cb73f69d87708e
Patrick Emond <emondpd@mcmaster.ca>
2020-07-28 14:55:01 -0400

    Marking start of version 2.6

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'salix';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.5';
+$SETTINGS['general']['version'] = '2.6';
 $SETTINGS['general']['build'] = '2147795';
 
 // the location of salix internal path
