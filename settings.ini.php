de88d0e5297f8fb35156349963734d9f275a4ffe
Patrick Emond <emondpd@mcmaster.ca>
2023-04-14 14:02:57 -0400

    Bumping to version 2.9

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'salix';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.8';
+$SETTINGS['general']['version'] = '2.9';
 $SETTINGS['general']['build'] = 'f572724';
 
 // the location of salix internal path
